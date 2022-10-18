bccd997cc725f65cd83688125633f5468055df3c
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2022-10-18 15:49:14 +0530

    update: graphql imp

--- graphql/Resolver.js
+++ graphql/Resolver.js
@@ -5,11 +5,22 @@ const resolvers = {
     getBooks() {
       return books;
     },
+    getBooksByLimit(_, { limit, offset }) {
+      // console.log(limit, offset);
+      return books.slice(offset, offset + limit);
+    },
+    getBookById(_parent, args) {
+      return books.filter((item) => item.id == args.id);
+    },
+    getBooksByGenre(_, { genres }) {
+      // console.log(genres);
+      return books.filter((item) => item.genres.includes(genres));
+    },
   },
   Mutation: {
     addBook(_parent, args) {
-      console.log("Args", args);
-      const newBook = args;
+      console.log("Args", args.input);
+      const newBook = args.input;
       books.push(newBook);
 
       return newBook;
--- graphql/TypeDef.js
+++ graphql/TypeDef.js
@@ -6,13 +6,49 @@ const { gql } = require("apollo-server-core");
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
+  # Interfaces define properties, methods, and events, which are the members of the interface
+  interface BookInterface {
+    id: ID!
+    title: String!
+    author: String!
+    year_written: Int!
+    edition: String!
+    price: Float!
+    genres: [String!]!
+  }
+
+  # "Book" type defines the queryable fields for every book in our data source.
+  type Book implements BookInterface {
+    id: ID!
+    title: String!
+    author: String!
+    year_written: Int!
+    edition: String!
+    price: Float!
+    genres: [String!]!
+    # rating: Int
+  }
+
+  # A special object type that groups a set of arguments together,
+  # and can then be used as an argument to another field
+  input BookInput {
+    id: ID
     title: String
     author: String
     year_written: Int
     edition: String
     price: Float
+    genres: [String]
+  }
+
+  enum Genre {
+    FANTASY
+    ROMANCE
+    HISTORICAL_FICTION
+    ACTION_AND_ADVENTURE
+    THRILLER_AND_SUSPENSE
+    MYSTERY
+    SCIENCE_FICTION
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -20,16 +56,13 @@ const typeDefs = gql`
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     getBooks: [Book]
+    getBooksByLimit(limit: Int, offset: Int): [Book]
+    getBookById(id: ID!): [Book]
+    getBooksByGenre(genres: String!): [Book]
   }
 
   type Mutation {
-    addBook(
-      title: String
-      author: String
-      year_written: Int
-      edition: String
-      price: Float
-    ): Book!
+    addBook(bookInput: BookInput!): Book!
   }
 `;
 
